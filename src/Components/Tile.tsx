6e1ff00ac4c5cfcbf77b7e4c13f2f787195651ec
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-03-31 18:22:53 +0200

    Add more styles

--- src/Components/Tile.tsx
+++ src/Components/Tile.tsx
@@ -1,7 +1,13 @@
 function Tile({id, TileClick, content}: TileProps) {
     return (
         <div className="Tile" onClick={() => {TileClick(id)}}>
-            {content}
+            {
+                content == 'x' ? (
+                    <span className="Cross">{content}</span>
+                ) : (
+                    <span className="Circle">{content}</span>
+                )
+            }
         </div>
     )
 }
